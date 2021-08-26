951a0569abb3f882a9e29890b8aa6b54e5c1ec9c
AshIsbitt <ashisbitt@icloud.com>
2021-08-26 19:07:42 +0100

    minor updates to gitignore and Ch12-play2048.py

--- PracticeProjects/ch12-play2048.py
+++ PracticeProjects/ch12-play2048.py
@@ -7,9 +7,6 @@ and over again. Write a program that will open the game at https://gabrielecirul
 sending up, right, down, and left keystrokes to automatically play the game.
 '''
 
-import webbrowser
-import requests
-import bs4
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -17,30 +14,30 @@ import time
 # Open 2048
 browser = webdriver.Firefox()
 gameSite = browser.get('https://gabrielecirulli.github.io/2048/')
-gameSoup = bs4.BeautifulSoup(gameSite.read(), 'html.parser')
 
 # Get game box
-gameElem = browser.find_element_by_class_name('game-container')
+gameElem = browser.find_element_by_tag_name('body')
 
 
 # Loop through sending up, right, down left
 while True:
     # Detect if the game has ended
     try:
+        print("Game Over")
         gameoverElem = browser.find_element_by_class_name(
             'game-message game-over')
 
         # Print to terminal final score
-        scoreElem = gameSoup.select('#score-container')
+        scoreElem = browser.find_element_by_class_name('score-container')
         print(f'Final score: {scoreElem[0].getText()}')
         break
 
     except:
         gameElem.send_keys(Keys.UP)
-        time.sleep(0.5)
+        time.sleep(0.2)
         gameElem.send_keys(Keys.RIGHT)
-        time.sleep(0.5)
+        time.sleep(0.2)
         gameElem.send_keys(Keys.DOWN)
-        time.sleep(0.5)
+        time.sleep(0.2)
         gameElem.send_keys(Keys.LEFT)
-        time.sleep(0.5)
+        time.sleep(0.2)
