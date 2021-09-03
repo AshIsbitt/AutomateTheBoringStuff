79aa687f70a0ae7d857164e1070ec6b4af9164be
AshIsbitt <ashisbitt@icloud.com>
2021-09-03 18:12:16 +0100

    Ch15 practice project complete

--- PracticeProjects/Ch15-CustomWordInvites.py
+++ PracticeProjects/Ch15-CustomWordInvites.py
@@ -12,19 +12,21 @@ doc = docx.Document('ch15-Invites.docx')
 guestList = guests.readlines()
 
 for i in range(len(guestList)):
-	# Write out text for invite
-	# Apply appearance
-	doc.add_paragraph('It would be a pleasure to have the company of', style='Custom1')
+    # Write out text for invite
+    # Apply appearance
+    doc.add_paragraph(
+        'It would be a pleasure to have the company of', style='Custom1')
 
-	# Add name from file
-	doc.add_paragraph(guestList[i], style='Custom2')
+    # Add name from file
+    doc.add_paragraph(guestList[i], style='Custom2')
 
-	doc.add_paragraph('at 11010 Memory Lane on the Evening of', style='Custom1')
-	doc.add_paragraph('April 1st', style='Custom3')
-	doc.add_paragraph("at 7 o'clock", style='Custom1')
+    doc.add_paragraph(
+        'at 11010 Memory Lane on the Evening of', style='Custom1')
+    doc.add_paragraph('April 1st', style='Custom3')
+    doc.add_paragraph("at 7 o'clock", style='Custom1')
 
-	# Add page break
-	doc.paragraphs[4].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
+    # Add page break
+    doc.paragraphs[i * 5].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
 
 # Save doc and close .txt
 doc.save('ch15-Invites.docx')
