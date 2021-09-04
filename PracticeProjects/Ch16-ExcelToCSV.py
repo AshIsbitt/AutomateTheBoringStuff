4c53100ebef9f4ad8db184f8fe60677f2a8ad427
AshIsbitt <ashisbitt@icloud.com>
2021-09-04 13:52:39 +0100

    Ch16 project exceltocsv

--- PracticeProjects/Ch16-ExcelToCSV.py
+++ PracticeProjects/Ch16-ExcelToCSV.py
@@ -1,34 +1,54 @@
 # Excel-to-CSV Converter
 
 import openpyxl
+from openpyxl.utils import get_column_letter
 import csv
 import os
 from pathlib import Path
 
-# Skip non-excel files
 for excelFile in os.listdir('./excelSpreadsheets/'):
+    # Skip non-excel files
+    p = Path(f'./excelSpreadsheets/{excelFile}')
+
+    if p.suffix != '.xlsx':
+        continue
+
     print(f'Opening {excelFile}...')
 
+    # Set spreadsheet as Spreadsheet object
     wb = openpyxl.load_workbook(os.path.join('excelSpreadsheets', excelFile))
 
+    # Create location for output
+    Path('./excelSpreadsheets/outputCSVfiles/').mkdir(exist_ok=True)
+
     # Loop through all sheets in workbook
-    for sheetName in wb.get_sheet_names():
-        sheet = wb.get_sheet_by_name(sheetName)
+    for sheetName in wb.sheetnames:
+        # Get sheet
+        sheet = wb[sheetName]
 
         # Create CSV filename from excel filename and sheet name
         filename = f"{str(excelFile[:-5])}-{str(sheet.title)}"
+        print(filename)
 
         # Create CSV writer
-        Path.mkdir('./excelSpreadsheets/outputCSVfiles', exist_ok=True)
-        csvFile = open(os.path.join('excelSpreadsheets', 'outputCSVfiles'), 'w')
+        csvFile = open(
+            f'./excelSpreadsheets/outputCSVfiles/{filename}.csv', 'w')
+        csvWriter = csv.writer(csvFile)
 
-    #     # Loop through every row in the sheet
-    #     for rowNum in range(1, sheet.max_row + 1):
-    #         rowData = []
+        # Loop through every row in the sheet
+        for rowNum in range(1, sheet.max_row + 1):
+            rowData = []
 
-    #         for colNum in range(1, sheet.max_column + 1):
-    #             # Append each cell to rowData
+            for colNum in range(1, sheet.max_column + 1):
+                # Append each cell to rowData
 
-    #             # write rowData list to CSV file
+                rowData.append(
+                    sheet[f'{get_column_letter(colNum)}{rowNum}'].value)
 
-    # csvFile.close()
+            # write rowData list to CSV file
+            print(f'rowdata = {rowData}')
+            csvWriter.writerow(rowData)
+
+    csvFile.close()
+
+print('Finished!')
